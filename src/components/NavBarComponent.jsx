3b3d6c5e8ad73a8ec37a2475fd2d82360d1b6d6c
Vaibhavi Rana <vvr25@njit.edu>
2024-12-15 17:56:04 -0500

    Updated Home screen

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -21,11 +21,12 @@ import {
   AccountCircleOutlined,
 } from "@mui/icons-material";
 import { useState } from "react";
+import { Link } from "react-router-dom"; // Import Link
 
 export default function NavBarComponent() {
   const [notificationAnchorEl, setNotificationAnchorEl] = useState(null);
   const [anchorEl, setAnchorEl] = useState(null);
-  // handleNotificationClicked
+    // handleNotificationClicked
   const open = Boolean(anchorEl);
   const notificationOpen = Boolean(notificationAnchorEl);
   const handleAvatarClicked = (event) => {
@@ -124,7 +125,7 @@ export default function NavBarComponent() {
                   </MenuItem>
                   <Divider />
 
-                  <MenuItem>
+                  <MenuItem component={Link} to="/settings">
                     <ListItemIcon>
                       <Settings fontSize="small" />
                     </ListItemIcon>
